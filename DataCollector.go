221630ed05799f977426dae9edf80008e5ce6f52
何雨航 <miswantism@gmail.com>
2017-10-07 15:11:44 +0800

    正式版放出！v1.0.0

--- DataCollector.go
+++ DataCollector.go
@@ -10,28 +10,29 @@ import (
 	"strings"
 )
 
-var doc = `实验数据处理程序
-
-*作者：
-  何雨航 <453542772@qq.com>
-*版本：
-  v1.0.0-go
-*介绍：
+var name = "实验数据处理程序"
+var author = "何雨航"
+var contact = "453542772@qq.com"
+var mainVersion = "v1.0.0-rc1"
+var doc = `*介绍：
   该脚本可以将分散于多个文件的实验数据处理成方便导入到OriginLab等各种表格程序的格式。
 *使用方法：
-  1. 依次将实验数据文件按顺序，拖动到本程序文件上，程序将自动导入实验数据并存入名为"cache.json"的缓存文件中（请不要手动删除它）；
-  2. 实验数据文件全部被程序读取之后，直接启动本程序。程序将自动清除缓存文件"cache.json"并将整理好的数据输出到程序旁边的名为："output.txt"的文件中；
-  3. 打开并用“Ctrl+A”全选"output.txt"中的数据并粘贴到表格中。
+  1. 依次将实验数据文件按顺序，拖动到本程序文件上，程序将自动导入实验数据并存入名为“cache.json”的缓存文件中（请不要手动删除它）；
+  2. 实验数据文件全部被程序读取之后，直接启动本程序。程序将自动清除缓存文件“cache.json”并将整理好的数据输出到程序旁边的名为：“output.txt”的文件中；
+  3. 打开并用“Ctrl+A”全选“output.txt”中的数据并粘贴到表格中。
 *注意事项：
   1. 本程序支持的实验数据文件格式有限，无法适用于所有试验数据文件，若需要作者支持更多的格式，请联系作者；
   2. 由于不同版本的操作系统的参数机制不同，推荐将数据文件按照顺序一个一个地拖动到本程序文件上进行读取，而不要同时拖动多个，否则会有概率导致读取顺序不理想；
-  3. 由于本程序会在旁边生成名为"cache.json"、"output.txt"文件，所以请不要把其他有价值的文件命名为这个并和本程序放在一起；
+  3. 由于本程序会在旁边生成名为“cache.json”、“output.txt”文件，所以请不要把其他有价值的文件命名为这个并和本程序放在一起；(￣▽￣)"
   4. 拖动操作是指类似于把文件框选并用鼠标左键按住拖动到文件夹的方式，把实验数据文件框选并用鼠标左键按住并拖动到本程序文件上；
-  5. 本程序的功能于Windows 10上测试通过，其他版本并未测试；
+  5. 本程序的功能于Windows 10上测试通过并支持同时读取44+份数据文件，其他系统版本并未测试；
   6. 请不要将不同种类的数据混合处理；
-  7. 数据读取后的表头来源于导入文件的文件名，请认真命名并养成好习惯。`
+  7. 数据读取后的表头来源于导入文件的文件名，请养成认真规范命名的好习惯；
+  8. 本程序仅用于实验，请勿用于生产环境。本程序可靠性未经过完整测试，请带着怀疑精神使用，并例行检验程序运行结果。
+  9. 有BUG？请联系作者！
+	`
 
-var debug = true
+var debug = false
 var cacheFileExist = checkFileExist(getCurrentPath() + "cache.json")
 
 // Item 通用
@@ -75,9 +76,15 @@ func main() {
 		analyse()
 		writeOutput()
 	case len(os.Args) == 1 && !cacheFileExist: // 显示文档
+		fmt.Println(name)
+		fmt.Println()
+		fmt.Println("作者：" + author + "<" + contact + ">")
+		fmt.Println("主程序版本：" + mainVersion)
+		fmt.Println("数据库版本：" + dbVersion)
+		fmt.Println()
 		fmt.Println(doc)
 	}
-	fmt.Println("程序结束")
+	fmt.Println("按Enter键退出程序")
 	fmt.Scanln()
 }
 
@@ -91,7 +98,6 @@ func readFile(filePathList []string) {
 	for i := 0; i < len(filePathList); i++ {
 		fileByte, err := ioutil.ReadFile(filePathList[i])
 		check(err)
-		os.Stdout.Write(fileByte)
 		newItem := new(Item)
 		newItem.Name = filePathList[i]
 		newItem.Data = fileByte
--- lib_format.go
+++ lib_format.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"strings"
 )
 
-// var matrix [][]string
+var dbVersion = "v171007(v2)"
 
 type DataPair struct {
 	X string
@@ -28,43 +27,17 @@ func Collect(cache Cache) [][]string {
 }
 func dissolve(cache Cache) {
 	for i := 0; i < len(cache.Data); i++ {
-		var dataPiece DataPiece
 		switch {
 		// F-7000 FL Spectrophotometer
 		case string(cache.Data[i].Data)[0:6] == "Sample":
-			var dataPair DataPair
-			tmpName := strings.Split(cache.Data[i].Name, "\\")
-			tmpNameLen := len(tmpName)
-			tmpNameName := tmpName[tmpNameLen-1]
-			tmpName = strings.Split(tmpNameName, ".")
-			tmpNameLen = len(tmpName)
-			tmpNameName = strings.Join(tmpName[0:tmpNameLen-1], ".")
-			dataPair.X, dataPair.Y = "Length", tmpNameName
-			dataPiece.Data = append(dataPiece.Data, dataPair)
-			lines := strings.Split(string(cache.Data[i].Data), "\r\n")
-			var flag = false
-			for j := 0; j < len(lines); j++ {
-				if flag {
-					if lines[j] == "" {
-						flag = false
-					} else {
-						tmp := strings.Split(lines[j], "\t")
-						dataPair.X, dataPair.Y = tmp[0], tmp[1]
-						dataPiece.Data = append(dataPiece.Data, dataPair)
-					}
-				} else {
-					if lines[j] == "nm\tData" {
-						flag = true
-					}
-				}
-			}
+			fl(cache, i)
+		// UV
+		case string(cache.Data[i].Data)[0:4] == "nm;A":
+			uv(cache, i)
 		}
-		dataList.Data = append(dataList.Data, dataPiece)
 	}
 }
 func arrange() [][]string {
-	fmt.Println(&dataList.Data[0].Data[0].X)
-	fmt.Println(&dataList.Data[0].Data[1].X)
 	x := len(dataList.Data) + 1
 	y := len(dataList.Data[0].Data)
 	// var matrix [][]string
@@ -81,4 +54,44 @@ func arrange() [][]string {
 		}
 	}
 	return matrix
+}
+
+// 数据库
+
+func stdDP(cache Cache, i int, prefix string, midfix string, suffix string) { // 标准处理流程
+	var dataPiece DataPiece
+	var dataPair DataPair
+	tmpName := strings.Split(cache.Data[i].Name, "\\")
+	tmpNameLen := len(tmpName)
+	tmpNameName := tmpName[tmpNameLen-1]
+	tmpName = strings.Split(tmpNameName, ".")
+	tmpNameLen = len(tmpName)
+	tmpNameName = strings.Join(tmpName[0:tmpNameLen-1], ".")
+	dataPair.X, dataPair.Y = "Length", tmpNameName
+	dataPiece.Data = append(dataPiece.Data, dataPair)
+	lines := strings.Split(string(cache.Data[i].Data), "\r\n")
+	var flag = false
+	for j := 0; j < len(lines); j++ {
+		if flag {
+			if lines[j] == suffix { // 需设定(可选)：数据后一排
+				flag = false
+			} else {
+				tmp := strings.Split(lines[j], midfix) // 需设定：数据间分隔符
+				dataPair.X, dataPair.Y = tmp[0], tmp[1]
+				dataPiece.Data = append(dataPiece.Data, dataPair)
+			}
+		} else {
+			if lines[j] == prefix { // 需设定：数据前一排
+				flag = true
+			}
+		}
+	}
+	dataList.Data = append(dataList.Data, dataPiece)
+}
+
+func fl(cache Cache, i int) { // 荧光（适用标准处理流程）
+	stdDP(cache, i, "nm\tData", "\t", "")
+}
+func uv(cache Cache, i int) { // 紫外（适用标准处理流程）
+	stdDP(cache, i, ";0.00 sec", ";", "")
 }
